1b558709b0c8ca226ddfffd853d73c052e7bc5da
yeeun <yeeeun@medicalai.com>
2023-07-25 18:07:19 +0900

    Initial Commit

---
+++ kurlyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kurlyClone
+//
+//  Created by 김예은 on 2023/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
