633bfbe7f7df1594f10d1a0e2f70c99f7b7693ba
yeeun <yeeeun910@gmail.com>
2023-08-02 05:26:30 +0900

    icon 및 color set 추가

--- kurlyClone/ViewController.swift
+++ kurlyClone/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	lazy var testImageView: UIImageView = {
+		// Set the size of UIImageView
+		let width: CGFloat = 100
+		let height: CGFloat = 100
+		
+		// Set x, y of imageview
+		let posX: CGFloat = (self.view.bounds.width - width)/2
+		let posY: CGFloat = (self.view.bounds.height - height)/2
+		
+		// Create UIImageview
+		let imageView = UIImageView(frame: CGRect(x: posX, y: posY, width: 100, height: 100))
+		
+		// Create UIImage
+		let myImage = UIImage(named: "location.fill")!
+		
+		// Set the image to UIImageView.
+		imageView.image = myImage
+		
+		return imageView
+	}()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+		// Add Test UIImageView to view
+		//self.view.addSubview(self.testImageView)
     }
 
 
