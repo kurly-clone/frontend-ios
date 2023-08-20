d46a4499430b06a4d9389f50fe75c9e5a2f9bcbc
yeeun <yeeeun910@gmail.com>
2023-08-20 16:42:55 +0900

    fix : pod 관련 merge conflict 오류 수정

--- kurlyClone/ViewControllers/PageViewControllers/Home/HomeVC.swift
+++ kurlyClone/ViewControllers/PageViewControllers/Home/HomeVC.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class HomeVC: UIViewController {
+	
+	private var viewControllers: Array<UIViewController> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
 
